c6f5c8f2fc2a5eb6947d8a0c148f5707ee7c6133
GreenyMushrooms <ethereal@sfr.fr>
2026-05-25 11:10:10 +0200

    Made code prettier

--- scripts/classes/character.gd
+++ scripts/classes/character.gd
@@ -1,7 +1,8 @@
 extends Node3D
 class_name CharacterAvatarMesh
 
-# These relative paths point exactly to its own local bone attachment children
+# You could make accessories/cosmetics from this class later on
+# Optimize this :sob:
 @onready var head_mesh: MeshInstance3D = $ObbyAvatar/Skeleton3D/head/Head
 @onready var torso_mesh: MeshInstance3D = $ObbyAvatar/Skeleton3D/torso/Torso
 @onready var left_arm_mesh: MeshInstance3D = $ObbyAvatar/Skeleton3D/leftarm/LeftArm
@@ -10,21 +11,17 @@ class_name CharacterAvatarMesh
 @onready var right_leg_mesh: MeshInstance3D = $ObbyAvatar/Skeleton3D/rightleg/RightLeg
 
 func _ready() -> void:
-	apply_saved_colors()
-
-func apply_saved_colors() -> void:
-	if GameManager and GameManager.data and GameManager.data.body_colors:
-		update_part_color("head", GameManager.data.body_colors.get("head", Color.WHITE))
-		update_part_color("torso", GameManager.data.body_colors.get("torso", Color.WHITE))
-		update_part_color("left_arm", GameManager.data.body_colors.get("left_arm", Color.WHITE))
-		update_part_color("right_arm", GameManager.data.body_colors.get("right_arm", Color.WHITE))
-		update_part_color("left_leg", GameManager.data.body_colors.get("left_leg", Color.WHITE))
-		update_part_color("right_leg", GameManager.data.body_colors.get("right_leg", Color.WHITE))
-
+	update_part_color("head", GameManager.data.body_colors.get("head", Color.WHITE))
+	update_part_color("torso", GameManager.data.body_colors.get("torso", Color.WHITE))
+	update_part_color("left_arm", GameManager.data.body_colors.get("left_arm", Color.WHITE))
+	update_part_color("right_arm", GameManager.data.body_colors.get("right_arm", Color.WHITE))
+	update_part_color("left_leg", GameManager.data.body_colors.get("left_leg", Color.WHITE))
+	update_part_color("right_leg", GameManager.data.body_colors.get("right_leg", Color.WHITE))
+	
 func update_part_color(part_name: String, new_color: Color) -> void:
 	var target_mesh: MeshInstance3D = null
 	
-	match part_name.to_lower():
+	match part_name:
 		"head": target_mesh = head_mesh
 		"torso": target_mesh = torso_mesh
 		"left_arm": target_mesh = left_arm_mesh
@@ -36,8 +33,5 @@ func update_part_color(part_name: String, new_color: Color) -> void:
 	
 	# Override to setup new colors
 	var mat = StandardMaterial3D.new()
-	mat.shading_mode = StandardMaterial3D.SHADING_MODE_PER_PIXEL
-	mat.roughness = 0.7
 	mat.albedo_color = new_color
-	
 	target_mesh.material_override = mat
